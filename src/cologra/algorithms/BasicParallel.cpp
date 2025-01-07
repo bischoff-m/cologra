6d2770b026b67a2f799f6978cc633b460036bc7e
Arthur Marcel Fibich <arthurfibich@gmail.com>
2025-01-07 20:31:57 +0100

    Distance-2 Coloring

--- src/cologra/algorithms/BasicParallel.cpp
+++ src/cologra/algorithms/BasicParallel.cpp
@@ -47,6 +47,47 @@ ColorType sequenceDependentGreedyColoring(
   return numColors;
 }
 
+ColorType sequenceDependentGreedyDist2Coloring(
+    Graph graph, ColorMap coloring, VertexIteratorRange indices) {
+  // Set all entries in the coloring to -1
+  for (auto node : boost::make_iterator_range(boost::vertices(graph)))
+    boost::put(coloring, node, -1);
+
+  // Iterate over all nodes in the given indices sequence
+  ColorType numColors = 0;
+  for (auto node : boost::make_iterator_range(
+           boost::make_permutation_iterator(
+               boost::vertices(graph).first, indices.first),
+           boost::make_permutation_iterator(
+               boost::vertices(graph).first, indices.second))) {
+    // Get the neighbors of the node
+    auto neighbors = boost::adjacent_vertices(node, graph);
+    std::vector<VertexType> neighborsneighbors = {};
+    for (auto neighbor : boost::make_iterator_range(neighbors)){
+      neighborsneighbors.push_back(neighbor);
+      for (auto nn : boost::make_iterator_range(boost::adjacent_vertices(neighbor, graph))){
+        neighborsneighbors.push_back(nn);
+      }
+    }
+
+    // Get the colors of the neighbors
+    vector<ColorType> neighborColors;
+    for (auto neighbor : neighborsneighbors)
+      neighborColors.push_back(coloring[neighbor]);
+
+    // Find the smallest color not used by any of its neighbors
+    ColorType color;
+    for (color = 0; color < boost::num_vertices(graph); color++)
+      if (find(neighborColors.begin(), neighborColors.end(), color) ==
+          neighborColors.end())
+        break;
+
+    boost::put(coloring, node, color);
+    numColors = max(numColors, color + 1);
+  }
+  return numColors;
+}
+
 ColorType BasicParallel::computeColoring(Graph graph, ColorMap coloring) {
   VertexIteratorRange identity_ordering = boost::vertices(graph);
 
@@ -58,6 +99,28 @@ ColorType BasicParallel::computeColoring(Graph graph, ColorMap coloring) {
     ColorMap local_coloring(&colorVec.front(), get(vertex_index, graph));
     ColorType local_numColors =
         sequenceDependentGreedyColoring(graph, local_coloring, indices);
+#pragma omp critical
+    {
+      if (local_numColors < numColors) {
+        coloring = local_coloring;
+        numColors = local_numColors;
+      }
+    }
+  }
+  return numColors;
+}
+
+ColorType BasicParallel::computeDist2Coloring(Graph graph, ColorMap coloring) {
+  VertexIteratorRange identity_ordering = boost::vertices(graph);
+
+  std::vector<VertexIteratorRange> index_iterators = {identity_ordering};
+  ColorType numColors = std::numeric_limits<ColorType>::max();
+#pragma omp parallel for reduction(min : numColors)
+  for (auto &indices : index_iterators) {
+    vector<ColorType> colorVec(boost::num_vertices(graph));
+    ColorMap local_coloring(&colorVec.front(), get(vertex_index, graph));
+    ColorType local_numColors =
+        sequenceDependentGreedyDist2Coloring(graph, local_coloring, indices);
 #pragma omp critical
     {
       if (local_numColors < numColors) {
