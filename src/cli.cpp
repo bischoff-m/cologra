e7ff5ebd6602c596b8885f43cdcd882952ccd073
Arthur Marcel Fibich <arthurfibich@gmail.com>
2025-01-09 22:44:32 +0100

    cli: corrected help, added degeneracy

--- src/cli.cpp
+++ src/cli.cpp
@@ -16,6 +16,7 @@
 #include "benchmark/entry.hpp"
 #include <boost/mpi.hpp>
 #include <boost/mpi/environment.hpp>
+#include "cologra/util/degeneracy.cpp"
 
 
 ColoringAlgorithm *createAlgorithm(string id, json params) {
@@ -37,7 +38,8 @@ int main(int argc, char const *argv[])
     argh::parser cmdl;
     cmdl.add_params({"-i", "--input"}); // pre-register "input" as a param: name + value
     cmdl.add_params({"-o", "--output", "--compress-to"}); // pre-register "output" as a param: name + value
-    cmdl.add_params({"-a", "--algorithm"}); // pre-register "output" as a param: name + value
+    cmdl.add_params({"-a", "--algorithm"}); // pre-register "algorithm" as a param: name + value
+    cmdl.add_params({"--use-degeneracy"}); // pre-register "algorithm" as a param: name + value
     cmdl.parse(argc, argv);
     if (cmdl[{"-h", "--help"}] || cmdl.params().size()==0)
     {
@@ -48,14 +50,14 @@ int main(int argc, char const *argv[])
         std::cout << std::endl;
         std::cout << "cologra_cli color -i <path_to_input_file> -o <path_to_output_file>,\t\t interprets the matrix as adjacency matrix of a graph, computes a coloring, prints the number of colors used and (if an outfile is given) writes the colored graph in TODO form" << std::endl;
         std::cout << "cologra_cli compress -i <path_to_input_file> -o <path_to_output_file>\t\t like color, only the file output is the compressed matrix instead of the colored graph" << std::endl;
-        std::cout << "bench\t\t prints this message" << std::endl;
         std::cout << "FLAGS:" << std::endl;
         std::cout << "-h, --help\t\t prints this message" << std::endl;
-        std::cout << "-h, --verbose\t\t prints this message" << std::endl;
+        std::cout << "-h, --verbose\t\t prints some stuff" << std::endl;
         std::cout << "-i, --input <path_to_file>\t\t specify input file" << std::endl;
-        std::cout << "-o, --output <path_to_file>, compress-to\t\t specify output file" << std::endl;
+        std::cout << "-o, --output, --compress-to <path_to_file>\t\t specify output file" << std::endl;
         std::cout << "-a, --algorithm <algo-name>\t\t which algorithm is used for the coloring" << std::endl;
-        std::cout << "--columnIntersectionGraph\t\t chenges the first step by creating a CIG instead of interpreting the matrix as adjacency matrix" << std::endl;
+        std::cout << "--use-degeneracy <k>\t\t computes the coloring in the k-degeneracy Graph. If the result is a " << std::endl;
+        std::cout << "--columnIntersectionGraph, --columnwise\t\t chenges the first step by creating a CIG instead of interpreting the matrix as adjacency matrix. For compression, this option compresses columnwise" << std::endl;
         std::cout << "--adjacencyGraph\t\t chenges the first step by creating a CIG instead of interpreting the matrix as adjacency matrix" << std::endl;
         return 0;
     }
@@ -65,7 +67,7 @@ int main(int argc, char const *argv[])
         std::cout << "The Output is verbose" << std::endl;
     }
 
-    if(cmdl(1) == "bench"){
+    if(cmdl(1).str() == "bench"){
         mpi::environment env(argc-1, argv+1);
         runBenchmark();
         return 0;
@@ -91,22 +93,34 @@ int main(int argc, char const *argv[])
     }
     Graph graph;
 
-    if(cmdl["--columnIntersectionGraph"]){
+    if(cmdl["--columnIntersectionGraph", "--columnwise"]){
         graph = columnIntersectionGraph(matrix);
     }
     else if(cmdl["--adjacencyGraph"]){
         graph = adjacencyGraph(matrix);
     }
     else{
-        std::cout << "No matrix interpretation given, using Adjacency Graph" << std::endl;
-        graph = adjacencyGraph(matrix);
+        if(cmdl(1).str() == "compress"){
+            std::cout << "No compression direction given, using columnwise" << std::endl;
+            graph = columnIntersectionGraph(matrix);
+        }
+        else{
+            std::cout << "No matrix interpretation given, using Adjacency Graph" << std::endl;
+            graph = adjacencyGraph(matrix);
+        }
+
     }
     if (make_verbose){
         std::cout << "Conversion into graph done\n";
     }
 
+    int k;
+    if (cmdl["--use-degeneracy"]>>k){
+        degeneracy_graph(graph, k);
+    }
 
-    json params = {};
+
+    json params = json::parse(cmdl("--params", "").str());
     ColoringAlgorithm * algo =  createAlgorithm(cmdl({"-a", "--algorithm"}).str(), params);
 
     std::vector<VerticesSizeType> colorVec(boost::num_vertices(graph));
@@ -115,6 +129,7 @@ int main(int argc, char const *argv[])
     ColorType numColors = algo->computeColoring(graph, coloring);
     std::cout << numColors << " colors used" << std::endl;
 
+    //TODO: rebuild coloring from degenerate graph
 
     // TODO: write result into file somehow
 
